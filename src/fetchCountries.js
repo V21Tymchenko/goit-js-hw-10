ff61111c621277d4f8c76f2ce154dd51e8563769
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-09-18 12:34:43 +0300

    refresh files

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,9 +3,9 @@ export default function fetchCountries(name) {
   const url = 'https://restcountries.com/v3.1/name/';
   const filter = '?fields=name,capital,population,flags,languages';
   return fetch(`${url}${name}${filter}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
+    if (response.ok) {
+      return response.json();
     }
-    return response.json();
+    throw new Error(response.statusText);
   });
 }
